060552acbebdc347000d7c44a7cc9faab45f323a
Makul0 <MukulKumar2027@gmail.com>
2024-06-22 06:05:09 +0530

    commit

--- src/app/publish/page.tsx
+++ src/app/publish/page.tsx
@@ -42,16 +42,20 @@ export default function Register() {
               </div>
               <p className="mt-2 text-md text-white">Write a few sentences about your work.</p>
             </div>
-            <div className="mt-4 flex text-xl leading-6 text-white ">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer bg-white text-black p-2 "
-                    >
-                      <span className="p-2">Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                    </label>
-                    
-                  </div>
+            <div className="mb-3 w-96 mt-4">
+          
+          <input
+            className="relative m-0 block w-full min-w-0 flex-auto rounded border border-white bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+            type="file"
+           id="formFile"
+          />
+          <label
+            htmlFor="formFile"
+            className="mt-2 inline-block text-white dark:text-neutral-200"
+          >
+          Upload well edited file!
+          </label>
+      </div>
            
             <div className="flex justify-end p-2">
               <Link href="">
